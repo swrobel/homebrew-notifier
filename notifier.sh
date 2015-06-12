a1c21c7e2be57e17c0adf0279b3522c09b2aa347
Alex Grant <alex@vermonster.com>
2015-06-12 16:55:20 -0400

    Don't notify if there are no updates

--- notifier.sh
+++ notifier.sh
@@ -3,26 +3,16 @@
 # Homebrew Update Notifier
 # Original Author: Chris Streeter <http://www.chrisstreeter.com>
 
-BREW_EXEC=$(which brew)
+BREW=$(which brew)
 TERMINAL_NOTIFIER=$(which terminal-notifier)
-NOTIF_ARGS="-sender com.apple.Terminal"
 
-$BREW_EXEC update > /dev/null 2>&1
-outdated=$($BREW_EXEC outdated --quiet)
-pinned=$($BREW_EXEC list --pinned)
+$BREW update > /dev/null 2>&1
+outdated=$($BREW outdated --quiet)
+pinned=$($BREW list --pinned)
 
-# Remove pinned formulae from the list of outdated formulae
 outdated=$(comm -1 -3 <(echo "$pinned") <(echo "$outdated"))
 
 if [ -n "$outdated" ]; then
-    if [ -e "$TERMINAL_NOTIFIER" ]; then
-        # No updates available
-        $TERMINAL_NOTIFIER "$NOTIF_ARGS" \
-            -title "No Homebrew Updates Available" \
-            -message "No updates available yet for any homebrew packages."
-    fi
-else
-    # We've got an outdated formula or two
     if [ -e "$TERMINAL_NOTIFIER" ]; then
         lc=$(echo "$outdated" | wc -l)
         outdated=$(echo "$outdated" | tail -"$lc")
@@ -34,8 +24,7 @@ $message"
             message="The following formulae are outdated:
 $message"
         fi
-        # Send to the Nofication Center
-        $TERMINAL_NOTIFIER "$NOTIF_ARGS" \
-            -title "Homebrew Update(s) Available" -message "$message"
+        $TERMINAL_NOTIFIER -sender com.apple.Terminal \
+            -title "Homebrew Updates Available" -message "$message"
     fi
 fi
