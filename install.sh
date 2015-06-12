1a7fbe3f9ff243f6cc90a19a1792c4e277796d48
Alex Grant <alex@vermonster.com>
2015-06-12 16:39:30 -0400

    Copy notifier and install scripts from gists

---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+NOTIFIER_URL=https://raw.githubusercontent.com/grantovich/homebrew-notifier/master/notifier.sh
+
+brew install terminal-notifier
+mkdir -p ~/.bin
+curl $NOTIFIER_URL > ~/.bin/brew-update-notifier.sh
+
+if crontab -l | grep -q 'brew-update-notifier'; then
+  echo 'Crontab entry already exists, skipping...'
+else
+  echo "0 11 * * * PATH=/usr/local/bin:\$PATH $(echo ~)/.bin/brew-update-notifier.sh" | crontab -
+fi
+
+echo
+echo "Notifier installed. You'll be notified of brew updates at 11am every day."
+echo "Checking for updates right now..."
+~/.bin/brew-update-notifier.sh
