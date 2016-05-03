45bac8273ad686379a3678868d9c9afc5e601f15
Alex Grant <grantovich@users.noreply.github.com>
2016-05-03 17:03:00 -0400

    Simplify logic in notifier script

--- notifier.sh
+++ notifier.sh
@@ -4,24 +4,14 @@ BREW=$(which brew)
 TERMINAL_NOTIFIER=$(which terminal-notifier)
 
 $BREW update > /dev/null 2>&1
+
 outdated=$($BREW outdated --quiet)
 pinned=$($BREW list --pinned)
+updatable=$(comm -1 -3 <(echo "$pinned") <(echo "$outdated"))
 
-outdated=$(comm -1 -3 <(echo "$pinned") <(echo "$outdated"))
-
-if [ -n "$outdated" ]; then
-    if [ -e "$TERMINAL_NOTIFIER" ]; then
-        lc=$(echo "$outdated" | wc -l)
-        outdated=$(echo "$outdated" | tail -n "$lc")
-        message=$(echo "$outdated" | head -n 5)
-        if [ "$outdated" != "$message" ]; then
-            message="Some of the outdated formulae are:
-$message"
-        else
-            message="The following formulae are outdated:
-$message"
-        fi
-        $TERMINAL_NOTIFIER -sender com.apple.Terminal \
-            -title "Homebrew Updates Available" -message "$message"
-    fi
+if [ -n "$updatable" ] && [ -e "$TERMINAL_NOTIFIER" ]; then
+    message="The following formulae are outdated:
+$updatable"
+    $TERMINAL_NOTIFIER -sender com.apple.Terminal \
+        -title "Homebrew Updates Available" -message "$message"
 fi
