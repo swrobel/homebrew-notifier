a4add275a9c706c6f65266006f3897ae79de6c72
Alex Grant <grantovich@users.noreply.github.com>
2016-05-03 16:52:38 -0400

    Use variables to reduce duplication in installer

--- install.sh
+++ install.sh
@@ -1,19 +1,21 @@
 #!/bin/sh
 
-NOTIFIER_URL=https://raw.githubusercontent.com/grantovich/homebrew-notifier/master/notifier.sh
+BASE_URL=https://raw.githubusercontent.com/grantovich/homebrew-notifier/master
+NOTIFIER_PATH=$HOME/.homebrew-notifier
+NOTIFIER_SCRIPT=$NOTIFIER_PATH/notifier.sh
 
-brew install terminal-notifier
-mkdir -p ~/.bin
-curl -fsS $NOTIFIER_URL > ~/.bin/brew-update-notifier.sh
-chmod +x ~/.bin/brew-update-notifier.sh
+brew list | grep -q "terminal-notifier" || brew install terminal-notifier
+mkdir -p "$NOTIFIER_PATH"
+curl -fsS $BASE_URL/notifier.sh > "$NOTIFIER_SCRIPT"
+chmod +x "$NOTIFIER_SCRIPT"
 
-if crontab -l | grep -q 'brew-update-notifier'; then
-  echo 'Crontab entry already exists, skipping...'
+if crontab -l | grep -q "notifier\.sh"; then
+  echo "Crontab entry already exists, skipping..."
 else
-  echo "0 11 * * * PATH=/usr/local/bin:\$PATH $HOME/.bin/brew-update-notifier.sh" | crontab -
+  echo "0 11 * * * PATH=/usr/local/bin:\$PATH $NOTIFIER_SCRIPT" | crontab -
 fi
 
 echo
 echo "Notifier installed. You'll be notified of brew updates at 11am every day."
 echo "Checking for updates right now..."
-~/.bin/brew-update-notifier.sh
+$NOTIFIER_SCRIPT
